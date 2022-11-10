84302af437a00323fa0b8e8383994ca9adfe4f91
affvini <vinicius.f.veras@gmail.com>
2022-11-10 18:18:56 -0300

    Novas aulas

---
+++ Aula JS/aula.js
@@ -0,0 +1,13 @@
+function meuNome(nome){    
+    console.log("Meu nome é" + nome);
+}
+meuNome("Vini");
+
+function verificarIdade(idade){
+    if (idade >= 18) {
+        console.log("Maior de idade");
+    } else {
+        console.log("menor de idade");
+    }
+}
+verificarIdade(18);
